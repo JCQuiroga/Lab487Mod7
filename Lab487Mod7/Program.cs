fd5666f3ae5d4f02bbe26c5a0b6378a498dcb8f1
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-24 12:29:32 +0100

    -H-

--- Lab487Mod7/Program.cs
+++ Lab487Mod7/Program.cs
@@ -13,13 +13,13 @@ namespace Lab487Mod7
         static void Main(string[] args)
         {
             ServiceHost sh = new ServiceHost(typeof(ServicioSaludoSb));
-
+            
             sh.AddServiceEndpoint(typeof(IServicioSaludoSb), new NetTcpBinding(), "net.tcp://localhost:9358/saludo");
             sh.AddServiceEndpoint(typeof(IServicioSaludoSb), new NetTcpRelayBinding(), ServiceBusEnvironment.CreateServiceUri("sb", "lab487mod7sb", "saludo")).Behaviors.Add(new TransportClientEndpointBehavior
             {
                 TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner", "xc/k9keil4yuD9dCw2+8UxsCnIz1+18mlqeTfX2KayU=")
             });
-
+            sh.Open();
             Console.WriteLine("Pulsa enter para terminar");
             Console.ReadLine();
             sh.Close();
