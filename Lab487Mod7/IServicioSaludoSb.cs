3cce827dcd74b8379c42380c29a6c7a06cb43166
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-24 10:35:23 +0100

    -+|+-

---
+++ Lab487Mod7/IServicioSaludoSb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Lab487Mod7
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServicioSaludoSb" in both code and config file together.
+    [ServiceContract(Namespace = "urn:ps")]
+    public interface IServicioSaludoSb : IDisposable
+    {
+        [OperationContract]
+        string GetSaludo(String idioma);
+    }
+}
---
+++ Lab487Mod7/IServicioSaludoServiceBusChannel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Lab487Mod7
+{
+    public interface IServicioSaludoServiceBusChannel : IServicioSaludoSb,
+IClientChannel
+    {}
+
+    public class ServicioSaludoSb : IServicioSaludoSb
+    {
+        Dictionary<String, String> saludos = new Dictionary<string, string>()
+        {
+            { "es","Buenos dias"},
+            {"en","Good morning"},
+            {"fr","Bon jour"},
+            { "de","Guten morgen"}
+        };
+
+        public string GetSaludo(string idioma)
+        {
+            if (saludos.ContainsKey(idioma)) return saludos[idioma];
+            return saludos["en"];
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
